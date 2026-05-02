483ffb0167df8e0321a8f6b14361fd8c43c4938d
MingqiLIU <mingqiliu608@gmail.com>
2026-05-02 18:11:11 +0800

    Task1 linking

--- Mingqi_Liu_20719335.m
+++ Mingqi_Liu_20719335.m
@@ -14,3 +14,13 @@ for i = 1:10
     writeDigitalPin(a, 'D10', 0);  % Turn LED off
     pause(0.5);                     % 0.5 seconds intervals
 end
+
+%% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+% (a) Temperature sensor connected to A0
+% Temperature coefficient (TC) = 10 mV/degC = 0.01 V/degC
+% Zero-degree voltage (V0) = 500 mV = 0.5 V
+% Formula: Temperature = (Voltage - V0) / TC
+
+ v = readVoltage(a, 'A0');
+ temp = (v - 0.5) / 0.01;
+ fprintf('Voltage: %.3f V, Temperature: %.1f C\n', v, temp);
